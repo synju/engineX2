452057665339c5ce0311b1e422340c2744616316
Unknown <charl.duplessis@derivco.co.za>
2018-02-22 14:47:38 +0200

    Finished Monster Bullets

--- src/spaceshooter/Resources.java
+++ src/spaceshooter/Resources.java
@@ -6,23 +6,23 @@ import javax.swing.ImageIcon;
 
 public class Resources {
 	// MONSTER IMAGES
-	public Image	converted		= new ImageIcon("res/spaceshooter/monsters/converted.png").getImage();
-	public Image	possessed		= new ImageIcon("res/spaceshooter/monsters/possessed.png").getImage();
-	public Image	minion			= new ImageIcon("res/spaceshooter/monsters/minion.png").getImage();
-	public Image	overlord		= new ImageIcon("res/spaceshooter/monsters/overlord.png").getImage();
-	public Image	demon			= new ImageIcon("res/spaceshooter/monsters/demon.png").getImage();
-	public Image	fallen_angel	= new ImageIcon("res/spaceshooter/monsters/fallen_angel.png").getImage();
-	public Image	devil			= new ImageIcon("res/spaceshooter/monsters/devil.png").getImage();
-
-	public Image	converted_bullet	= new ImageIcon("res/spaceshooter/monsters/converted_bullet.png").getImage();
-	public Image	possessed_bullet	= new ImageIcon("res/spaceshooter/monsters/possessed_bullet.png").getImage();
-	public Image	minion_bullet		= new ImageIcon("res/spaceshooter/monsters/minion_bullet.png").getImage();
-	public Image	overlord_bullet		= new ImageIcon("res/spaceshooter/monsters/overlord_bullet.png").getImage();
-	public Image	demon_bullet		= new ImageIcon("res/spaceshooter/monsters/demon_bullet.png").getImage();
+	public Image	converted						= new ImageIcon("res/spaceshooter/monsters/converted.png").getImage();
+	public Image	possessed						= new ImageIcon("res/spaceshooter/monsters/possessed.png").getImage();
+	public Image	minion							= new ImageIcon("res/spaceshooter/monsters/minion.png").getImage();
+	public Image	overlord						= new ImageIcon("res/spaceshooter/monsters/overlord.png").getImage();
+	public Image	demon								= new ImageIcon("res/spaceshooter/monsters/demon.png").getImage();
+	public Image	fallen_angel				= new ImageIcon("res/spaceshooter/monsters/fallen_angel.png").getImage();
+	public Image	devil								= new ImageIcon("res/spaceshooter/monsters/devil.png").getImage();
+	
+	public Image	converted_bullet		= new ImageIcon("res/spaceshooter/monsters/converted_bullet.png").getImage();
+	public Image	possessed_bullet		= new ImageIcon("res/spaceshooter/monsters/possessed_bullet.png").getImage();
+	public Image	minion_bullet				= new ImageIcon("res/spaceshooter/monsters/minion_bullet.png").getImage();
+	public Image	overlord_bullet			= new ImageIcon("res/spaceshooter/monsters/overlord_bullet.png").getImage();
+	public Image	demon_bullet				= new ImageIcon("res/spaceshooter/monsters/demon_bullet.png").getImage();
 	public Image	fallen_angel_bullet	= new ImageIcon("res/spaceshooter/monsters/fallen_angel_bullet.png").getImage();
-	public Image	devil_bullet		= new ImageIcon("res/spaceshooter/monsters/devil_bullet.png").getImage();
-
+	public Image	devil_bullet				= new ImageIcon("res/spaceshooter/monsters/devil_bullet.png").getImage();
+	
 	// PLAYER IMAGES
-	public Image	playerBullet	= new ImageIcon("res/spaceshooter/player/playerBullet.png").getImage();
-	public Image	player			= new ImageIcon("res/spaceshooter/player/player.png").getImage();
+	public Image	playerBullet				= new ImageIcon("res/spaceshooter/player/playerBullet.png").getImage();
+	public Image	player							= new ImageIcon("res/spaceshooter/player/player.png").getImage();
 }
